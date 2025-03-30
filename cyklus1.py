5c9ce5464e4af3e66a77c763caf486af5bcfacb6
Vlakmeister72 <mates8blaha@seznam.cz>
2025-03-30 15:45:18 +0200

    Hodina o cyklusech

---
+++ cyklus1.py
@@ -0,0 +1,18 @@
+saluto = "Ciao a tutti!"
+i = 1
+
+for lettera in saluto:
+    print(lettera)
+    i = i + 1
+
+print (f"Počet znaků je: {i}")
+
+
+zacatek = 3
+konec = 10
+
+for x in range(zacatek, konec):
+    print(x)
+
+for x in range(zacatek, konec, 2):
+    print(x)
